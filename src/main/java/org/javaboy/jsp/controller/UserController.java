7379402e2bccf01932172ab2d3df50ab97a2e9ef
zhanghuipeng <1727149886@qq.com>
2020-04-04 14:25:51 +0800

    first

---
+++ src/main/java/org/javaboy/jsp/controller/UserController.java
@@ -0,0 +1,15 @@
+package org.javaboy.jsp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+    @GetMapping("get")
+    public String get(String username,Model model){
+        model.addAttribute("username",username);
+        return "hello";
+    }
+}
